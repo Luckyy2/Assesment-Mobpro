9d2a487cc0891c1bba58f7fd42a619d4e9bf4523
Bimo <bimolp@student.telkomuniversity.ac.id>
2023-03-30 17:32:31 +0700

    Menambahkan View Binding dan Pembuatan Layout

--- app/src/main/java/org/d3if3001/movies/MainActivity.kt
+++ app/src/main/java/org/d3if3001/movies/MainActivity.kt
@@ -2,10 +2,15 @@ package org.d3if3001.movies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
+import org.d3if3001.movies.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
