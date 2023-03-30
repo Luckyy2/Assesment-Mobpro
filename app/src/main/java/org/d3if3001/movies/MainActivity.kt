88b9525494202b8e92d5e9c7eca8ab1c9853cb14
Bimo <bimolp@student.telkomuniversity.ac.id>
2023-03-30 17:41:35 +0700

    Menambahkan fungsi agar aplikasi bejalan sesuai dengan fungsinya

--- app/src/main/java/org/d3if3001/movies/MainActivity.kt
+++ app/src/main/java/org/d3if3001/movies/MainActivity.kt
@@ -12,5 +12,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnHitung.setOnClickListener {
+            val harga = binding.hargaInp.text.toString().trim()
+            val diskon = binding.diskonInp.text.toString().trim()
+            var totalDiskon = 0
+            var hargaBaru = 0
+
+            when{
+                TextUtils.isEmpty(harga) -> {
+                    Toast.makeText(this,"Harga belum ada!", Toast.LENGTH_SHORT).show()
+                    binding.hargaInp.requestFocus()
+                }
+                TextUtils.isEmpty(diskon) -> {
+                    Toast.makeText(this,"Diskon belum ada!", Toast.LENGTH_SHORT).show()
+                    binding.diskonInp.requestFocus()
+                }
+                else -> {
+                    totalDiskon = harga.toInt() * diskon.toInt() / 100
+                    hargaBaru = harga.toInt() - totalDiskon
+                    binding.tvTotal.text = hargaBaru.toString()
+                }
+            }
+        }
     }
 }
